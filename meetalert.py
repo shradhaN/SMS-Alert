9673c6cd1a34062921872995c2161b3aa3e2c275
Amit <meamitkc@gmail.com>
2017-12-16 18:18:32 +0545

    Apply PEP8 formatting

--- meetalert.py
+++ meetalert.py
@@ -1,38 +1,40 @@
+import time
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 from config import accounts
 
-import time
 
-#Message
+# Message
 message = raw_input("message")
 
 
-#phone numbers of club members
+# Phone numbers of club members
 group1 = accounts[0]['group']
 group2 = accounts[1]['group']
 
-#Login
-def login(username,password,group):
-	driver = webdriver.Firefox()
-	driver.get("http://www.meet.net.np/meet/")
-	driver.find_element_by_class_name("elgg-input-text").send_keys(username)
-	driver.find_element_by_class_name("elgg-input-password").send_keys(password)
-	driver.find_element_by_id("loginImage").click()
-	time.sleep(1)
 
+def login(username, password, group):
+    """Login to NTC Meet."""
+    driver = webdriver.Firefox()
+    driver.get("http://www.meet.net.np/meet/")
+    driver.find_element_by_class_name("elgg-input-text").send_keys(username)
+    driver.find_element_by_class_name(
+        "elgg-input-password").send_keys(password)
+    driver.find_element_by_id("loginImage").click()
+    time.sleep(1)
 
-	#Send Message
-	driver.get("http://www.meet.net.np/meet/sms/sms")
-	driver.find_element_by_name("sendsmsbutton").click()
-	time.sleep(1)
-	driver.find_element_by_name("recipient").send_keys(group)
-	driver.find_element_by_id("message").send_keys(message)
-	driver.find_element_by_name("sendbutton").click()
-	#close the browser
-	time.sleep(1)
-	driver.quit()
+    # Send Message
+    driver.get("http://www.meet.net.np/meet/sms/sms")
+    driver.find_element_by_name("sendsmsbutton").click()
+    time.sleep(1)
+    driver.find_element_by_name("recipient").send_keys(group)
+    driver.find_element_by_id("message").send_keys(message)
+    driver.find_element_by_name("sendbutton").click()
+
+    # Close the browser
+    time.sleep(1)
+    driver.quit()
 
 login(accounts[0]['username'], accounts[0]['password'], group1)
 time.sleep(1)
